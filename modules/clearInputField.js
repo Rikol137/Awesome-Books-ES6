6ea8022f4eed4da547432b6053d122041fd209f4
Rikol137 <rikolkelvin@gmail.com>
2022-11-07 12:48:06 +0300

    This is the Main folder with exports of JS files

---
+++ modules/clearInputField.js
@@ -0,0 +1,8 @@
+// eslint-disable-next-line import/extensions
+import { addBookForm } from './HtmlElements.js';
+
+// eslint-disable-next-line import/prefer-default-export
+export const clearField = () => {
+  addBookForm.bookTitle.value = '';
+  addBookForm.bookAuthor.value = '';
+};
